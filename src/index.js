311c73db01dcada8a227dbd7a9a9fcf73782cb58
adamsJS23 <adamaguebre@yahoo.fr>
2023-08-08 10:03:34 +0200

    coding challenge #1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "../src/index.css";
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <Intro />
+      <Skills />
+    </div>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Avatar() {
+  return <img src="spinaci.jpg" alt="Adam foto" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h3>Guebre Pooya Adama</h3>
+      <p>
+        Full Stack web developer and teacher at Udemy. When I'm not coding or
+        preparing a course, I like to play board, games, to cook(and eat), or to
+        just enjoy the portuguese sun at the beach
+      </p>
+    </div>
+  );
+}
+
+function Skills() {
+  return (
+    <div className="skills">
+      <span className="html">HTML+CSS</span>
+      <span className="javascript">Javascript</span>
+      <span className="react">React</span>
+      <span className="svelte">Svelte</span>
+      <span className="webdesign">Webdesign</span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
